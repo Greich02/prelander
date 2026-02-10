774e9e31d5d5044d97a850466b01b5fef60de59c
unknown <ugbongboui@gmail.com>
2026-02-10 10:41:08 +0100

    pixel cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.ads-twitter.com https://analytics.twitter.com;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data: https://static.ads-twitter.com https://ads-twitter.com https://analytics.twitter.com;
+      font-src 'self';
+      connect-src 'self' https://ads-api.twitter.com https://analytics.twitter.com https://static.ads-twitter.com;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
+    `;
+
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
