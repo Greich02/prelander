54a024da47414d88f17b7e03903625690632ef69
unknown <ugbongboui@gmail.com>
2026-02-10 11:11:21 +0100

    pixel fix 4

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,37 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' 
+        https://static.ads-twitter.com 
+        https://ads-twitter.com 
+        https://ads-api.twitter.com 
+        https://analytics.twitter.com
+        https://www.googletagmanager.com
+        https://www.google-analytics.com;
+      script-src-elem 'self' 'unsafe-inline'
+        https://static.ads-twitter.com 
+        https://ads-twitter.com 
+        https://ads-api.twitter.com 
+        https://analytics.twitter.com
+        https://www.googletagmanager.com
+        https://www.google-analytics.com;
       style-src 'self' 'unsafe-inline';
-      img-src 'self' blob: data: https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://t.co;
+      img-src 'self' blob: data: 
+        https://static.ads-twitter.com 
+        https://ads-twitter.com 
+        https://ads-api.twitter.com 
+        https://analytics.twitter.com
+        https://t.co
+        https://www.google-analytics.com
+        https://www.googletagmanager.com;
       font-src 'self';
-      connect-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
+      connect-src 'self' 
+        https://static.ads-twitter.com 
+        https://ads-twitter.com 
+        https://ads-api.twitter.com 
+        https://analytics.twitter.com
+        https://www.google-analytics.com
+        https://www.googletagmanager.com;
       object-src 'none';
       base-uri 'self';
       form-action 'self';
