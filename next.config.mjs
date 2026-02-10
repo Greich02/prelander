96aab98c1ac703d928b4a820f81df098231fca03
unknown <ugbongboui@gmail.com>
2026-02-10 10:48:40 +0100

    pixel fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.ads-twitter.com https://analytics.twitter.com;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
       style-src 'self' 'unsafe-inline';
-      img-src 'self' blob: data: https://static.ads-twitter.com https://ads-twitter.com https://analytics.twitter.com;
+      img-src 'self' blob: data: https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://t.co;
       font-src 'self';
-      connect-src 'self' https://ads-api.twitter.com https://analytics.twitter.com https://static.ads-twitter.com;
+      connect-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
       object-src 'none';
       base-uri 'self';
       form-action 'self';
