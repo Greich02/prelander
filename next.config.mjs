c89b00eba8f430d79000040be319f5b8232a81dc
unknown <ugbongboui@gmail.com>
2026-02-20 16:03:36 +0100

    event initf

--- next.config.mjs
+++ next.config.mjs
@@ -3,28 +3,26 @@ const nextConfig = {
   reactCompiler: true,
 
   async headers() {
-    const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.redditstatic.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com;
-      script-src-elem 'self' 'unsafe-inline' https://www.redditstatic.com https://static.ads-twitter.com https://ads-twitter.com https://www.googletagmanager.com;
-      style-src 'self' 'unsafe-inline';
-      img-src 'self' blob: data: https://www.redditstatic.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://t.co https://www.google-analytics.com https://www.googletagmanager.com;
-      font-src 'self';
-      connect-src 'self' https://events.redditmedia.com https://www.redditstatic.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.google-analytics.com https://www.googletagmanager.com;
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      upgrade-insecure-requests;
-    `;
-
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.redditstatic.com https://static.ads-twitter.com https://www.googletagmanager.com https://www.google-analytics.com",
+              "script-src-elem 'self' 'unsafe-inline' https://www.redditstatic.com https://static.ads-twitter.com https://www.googletagmanager.com",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' blob: data: https: https://www.redditstatic.com",
+              "font-src 'self' data:",
+              "connect-src 'self' https://events.redditmedia.com https://www.redditstatic.com https://static.ads-twitter.com https://ads-twitter.com https://www.google-analytics.com https://www.googletagmanager.com",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+              "frame-ancestors 'none'",
+              "upgrade-insecure-requests",
+            ].join('; '),
           },
         ],
       },
