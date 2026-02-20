fc3b5b192cefa11107d0cab7847dbd6fbb96fa2a
unknown <ugbongboui@gmail.com>
2026-02-20 15:05:44 +0100

    pixel reddit inline

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import { Playfair_Display, Inter, Cormorant_Garamond } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 // Configuration des polices
@@ -34,83 +33,47 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        {/* REDDIT PIXEL - BASE CODE */}
+        {/* REDDIT PIXEL - VERSION INLINE GARANTIE */}
         {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        <Script
-          id="reddit-pixel-base"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(w,d){
-                if(!w.rdt){
-                  var p=w.rdt=function(){
-                    p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)
-                  };
-                  p.callQueue=[];
-                  var t=d.createElement("script");
-                  t.src="https://www.redditstatic.com/ads/pixel.js";
-                  t.async=!0;
-                  var s=d.getElementsByTagName("script")[0];
-                  s.parentNode.insertBefore(t,s)
-                }
-              }(window,document);
-              
-              // Initialize Reddit Pixel with your ID
-              rdt('init','a2_ii7jiw9uywto', {"optOut":false,"useDecimalCurrencyValues":true});
-              
-              // Track PageVisit on all pages
-              rdt('track', 'PageVisit');
-              
-              console.log('✅ Reddit Pixel initialized: a2_ii7jiw9uywto');
+              !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_ii7jiw9uywto');rdt('track', 'PageVisit');console.log('✅ Reddit Pixel loaded');
             `,
           }}
         />
 
         {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        {/* X PIXEL - BASE CODE */}
+        {/* X PIXEL */}
         {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        <Script
-          id="twitter-pixel-base"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(e,t,n,s,u,a){
-                e.twq||(s=e.twq=function(){
-                  s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-                },
-                s.version='1.1',s.queue=[],
-                u=t.createElement(n),u.async=!0,
-                u.src='https://static.ads-twitter.com/uwt.js',
-                a=t.getElementsByTagName(n)[0],
-                a.parentNode.insertBefore(u,a))
-              }(window,document,'script');
-              
-              twq('config','r1bmm');
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments)},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','r1bmm');
+            `,
+          }}
+        />
+
+        {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
+        {/* GOOGLE ANALYTICS */}
+        {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WLXVWP8KZM"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WLXVWP8KZM');
             `,
           }}
         />
       </head>
       
-      <body
-        className={`${inter.variable} ${playfair.variable} ${cormorant.variable} antialiased`}
-      >
-        {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        {/* GOOGLE ANALYTICS */}
-        {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-WLXVWP8KZM"
-          strategy="afterInteractive"
-        />
-        
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-WLXVWP8KZM');
-          `}
-        </Script>
-
+      <body className={`${inter.variable} ${playfair.variable} ${cormorant.variable} antialiased`}>
         {children}
       </body>
     </html>
